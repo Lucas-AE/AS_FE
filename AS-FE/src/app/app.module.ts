e18493b2b924c5df8529c85269d4a3de28b2aefc
Joshi Vermeire <joshi.vermeire@qualityatspeed.be>
2018-09-13 14:28:46 +0200

    Add basic routing

--- AS-FE/src/app/app.module.ts
+++ AS-FE/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './menu/header/header.component';
+import { OverviewPageComponent } from './overview/overview-page/overview-page.component';
+import { DetailPageComponent } from './detail/detail-page/detail-page.component';
+
+const appRoutes: Routes = [
+  { path: 'detail', component: DetailPageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: OverviewPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    OverviewPageComponent,
+    DetailPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
