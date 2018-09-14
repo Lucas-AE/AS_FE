8111949c7d3c45bf52711919677fe82e970bc04c
Joshi Vermeire <joshi.vermeire@qualityatspeed.be>
2018-09-14 09:21:59 +0200

    Added bootstrap and bootstrap widgets

--- AS-FE/src/app/app.module.ts
+++ AS-FE/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './menu/header/header.component';
@@ -42,7 +43,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [
