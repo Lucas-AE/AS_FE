f4fd77b6b7dd717f79f257f2ec8f2a06514f1a3a
Lucas <lucas.bruggeman@ae.be>
2018-09-26 08:20:53 +0200

    hard-coded definitions of segments

--- AS-FE/src/app/overview/segments-overview/segments-overview.component.ts
+++ AS-FE/src/app/overview/segments-overview/segments-overview.component.ts
@@ -12,6 +12,8 @@ export class SegmentsOverviewComponent {
   accounts: AE_Account[];
 
   segments: string[] = ["Startup", "Scale-up", "Delivery", "Innovatief", "Tactisch", "Afbouw"];
+  definitions: string[] = ["Eerste opdracht bij nieuwe klant", "Geloof in evolutie naar delivery", "Strategisch partnership en intense lange termijn samenwerking",
+   "Geen delivery potentieel maar AE kan werken op een innovatief project en reference case uitbouwen", "Geen delivery potentieel maar AE is actief omwille van tactische redenen (referentie klan,persoonlijk fit,benchoptimalisatie,relatie,..)", "Consultants worden gecontroleerd uitgefaseerd tvv andere klanten."];
 
   constructor() { }
 
